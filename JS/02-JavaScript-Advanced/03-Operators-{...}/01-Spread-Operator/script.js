d5884c53beadb201f13fb46a85cd62f487d5d9af
Vinayak Yadav <netgoluyadav337@gmail.com>
2025-09-18 12:21:18 +0530

    Spread-Operator & Rest-Operator done

---
+++ JS/02-JavaScript-Advanced/03-Operators-{...}/01-Spread-Operator/script.js
@@ -0,0 +1,9 @@
+const arr1 = [1, 2];
+const arr2 = [3, 4];
+const combined = [...arr1, ...arr2];
+console.log(combined);
+
+const obj1 = { a: 1 };
+const obj2 = { b: 2 };
+const merged = { ...obj1, ...obj2 };
+console.log(merged);
---
+++ JS/02-JavaScript-Advanced/03-Operators-{...}/02-Rest-Operator/script.js
@@ -0,0 +1,6 @@
+function showNames(first, ...last) {
+  console.log("First name:", first);
+  console.log("Last name:", last);
+}
+
+showNames("Vinayak", "Yadav");
