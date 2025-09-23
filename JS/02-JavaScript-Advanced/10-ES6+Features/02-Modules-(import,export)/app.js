24d6b244d52b5721eabf0e8054c2cf2e2b4ca675
Vinayak Yadav <netgoluyadav337@gmail.com>
2025-09-23 16:07:03 +0530

    JavaScript Modules concept done

---
+++ JS/02-JavaScript-Advanced/10-ES6+Features/02-Modules-(import,export)/app.js
@@ -0,0 +1,5 @@
+import subtract, { add, PI } from "./math.js";
+
+console.log(add(2, 3)); // 5
+console.log(PI); // 3.14
+console.log(subtract(5, 2)); // 3
---
+++ JS/02-JavaScript-Advanced/10-ES6+Features/02-Modules-(import,export)/math.js
@@ -0,0 +1,10 @@
+export function add(a, b) {
+  return a + b;
+}
+
+export const PI = 3.14;
+
+// or we can use default export also
+export default function subtract(a, b) {
+  return a - b;
+}
