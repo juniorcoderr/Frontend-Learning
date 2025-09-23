979c3aaf756cc2e149f7a899a735c9e3b5fc80d4
Vinayak Yadav <netgoluyadav337@gmail.com>
2025-09-23 14:25:30 +0530

    OOP concept ABSTRACTION done

---
+++ JS/02-JavaScript-Advanced/09-OOP/Abstraction/script.js
@@ -0,0 +1,20 @@
+function Employee(name, age, baseSalary) {
+  this.name = name;
+  this.age = age;
+  this.baseSalary = baseSalary;
+
+  let monthlyBonus = 1000;
+
+  let calculateFinalSalary = function () {
+    let finalSalary = baseSalary + monthlyBonus;
+    console.log("Final Salary is : " + finalSalary);
+  };
+
+  this.getEmpDetails = function () {
+    console.log("Name : " + this.name + " | Age : " + this.age);
+    calculateFinalSalary();
+  };
+}
+
+let emp1 = new Employee("Vinayak", 22, 2000);
+emp1.getEmpDetails();
