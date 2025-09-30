df0f3d29efcb4edaa501763b79f7f3acebc1893b
Vinayak Yadav <netgoluyadav337@gmail.com>
2025-09-30 17:06:54 +0530

    Modules: Import/Export Syntax done

---
+++ TS/06-Modules/01-Named-Exports/main.ts
@@ -0,0 +1,8 @@
+import { add, subtract } from "./mathUtils";
+
+console.log(add(5, 3)); // 8
+console.log(subtract(5, 3)); // 2
+
+// import renaming
+// import { add as sum } from "./mathUtils";
+// console.log(sum(10, 20));
---
+++ TS/06-Modules/02-Default-Exports/index.ts
@@ -0,0 +1,2 @@
+import greet from "./greet"; // no { }
+console.log(greet("Vinayak"));
