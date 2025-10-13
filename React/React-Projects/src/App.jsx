43c7675b968c235717c63d2fce0c956ab9f29c72
Vinayak Yadav <netgoluyadav337@gmail.com>
2025-10-13 13:29:20 +0530

    Implement the mini-project-02

--- React/React-Projects/src/App.jsx
+++ React/React-Projects/src/App.jsx
@@ -1,9 +1,11 @@
-import Todo from "./01-Todo/Todo";
+// import Todo from "./01-Todo/Todo";
+import Meals from "./02-Meals-API/Meals";
 
 const App = () => {
   return (
     <div>
-      <Todo />
+      {/* <Todo /> */}
+      <Meals />
     </div>
   );
 };
