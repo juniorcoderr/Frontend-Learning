83e1c0e0346f23e90d8b1a2f7817bf1e2d128efc
Vinayak Yadav <netgoluyadav337@gmail.com>
2025-10-14 13:51:17 +0530

    Implement the mini-project-07

--- React/React-Projects/src/App.jsx
+++ React/React-Projects/src/App.jsx
@@ -3,8 +3,10 @@
 // import Calculator from "./03-Calculator/Calculator";
 // import ToggleBackgroundColor from "./04-ToggleBackgroundColor/ToggleBackgroundColor";
 // import HiddenSearchBar from "./05-HiddenSearchBar/HiddenSearchBar";
+// import Testimonials from "./06-Testimonials/Testimonials";
 
-import Testimonials from "./06-Testimonials/Testimonials";
+import Accordion from "./07-Accordions/Accordion";
+import { accordionData } from "./07-Accordions/utils/content";
 
 const App = () => {
   return (
@@ -14,7 +16,12 @@ const App = () => {
       {/* <Calculator /> */}
       {/* <ToggleBackgroundColor /> */}
       {/* <HiddenSearchBar /> */}
-      <Testimonials />
+      {/* <Testimonials /> */}
+      <div>
+        {accordionData.map(({ title, content }) => (
+          <Accordion title={title} content={content} />
+        ))}
+      </div>
     </div>
   );
 };
