daffda2fd07a02e1cc9ceb98246dcba3501c612a
Vinayak Yadav <netgoluyadav337@gmail.com>
2025-10-14 12:43:08 +0530

    Implement the mini-project-06

--- React/React-Projects/src/App.jsx
+++ React/React-Projects/src/App.jsx
@@ -2,8 +2,9 @@
 // import Meals from "./02-Meals-API/Meals";
 // import Calculator from "./03-Calculator/Calculator";
 // import ToggleBackgroundColor from "./04-ToggleBackgroundColor/ToggleBackgroundColor";
+// import HiddenSearchBar from "./05-HiddenSearchBar/HiddenSearchBar";
 
-import HiddenSearchBar from "./05-HiddenSearchBar/HiddenSearchBar";
+import Testimonials from "./06-Testimonials/Testimonials";
 
 const App = () => {
   return (
@@ -12,7 +13,8 @@ const App = () => {
       {/* <Meals /> */}
       {/* <Calculator /> */}
       {/* <ToggleBackgroundColor /> */}
-      <HiddenSearchBar />
+      {/* <HiddenSearchBar /> */}
+      <Testimonials />
     </div>
   );
 };
